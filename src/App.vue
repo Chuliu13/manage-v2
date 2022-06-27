33c29b730d4b39a220a211d9b5b8893e58d3b842
Chuliu13 <2311629049@qq.com>
2022-06-27 18:24:54 +0800

    1.0

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+
+  </div>
+</template>
+
+<script>
+// import Home from './components/Home.vue'
+// import Login from './components/Login.vue'
+
+export default {
+}
+</script>
+
+<style lang="scss">
+@import url('./assets/css/reset.css');
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="hello">
+    <h1>hello</h1>
+    <el-button type="primary">hello</el-button>
+    <el-button type="info">hello</el-button>
+    <el-button type="danger">hello</el-button>
+    <el-button type="success">hello</el-button>
+    <el-tag style="margin:10px">hahahaha</el-tag>
+    <i class="fa fa-users"></i>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+}
+</script>
+
+
+<style lang="scss">
+.hello{
+  background: yellow;
+  .el-button{
+    color: red;
+  }
+}
+</style>
