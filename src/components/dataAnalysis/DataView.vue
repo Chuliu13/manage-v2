82e60b70b1b7a50fef5073537f840df0f79e48cb
Chuliu13 <2311629049@qq.com>
2022-07-03 17:26:05 +0800

    6.5数据概览

--- src/components/dataAnalysis/DataView.vue
+++ src/components/dataAnalysis/DataView.vue
@@ -1,5 +1,94 @@
 <template>
-    <div>
-        数据概览    
-    </div>
+  <div class="data-view">
+    <el-card>
+      <div id="main1"></div>
+    </el-card>
+    <el-card>
+      <div id="main2"></div>
+    </el-card>
+  </div>
 </template>
+
+<script>
+import { dataview } from '@/api/api.js'
+export default {
+  data() {
+    return {};
+  },
+
+    created(){
+        // 第二个图标的绘制
+        dataview().then(res => {
+            console.log(res)
+            if(res.data.status === 200) {
+                let {legend, xAxis, series} = res.data.data
+                this.draw(legend, xAxis, series)
+            }
+        }).catch(err => {
+            throw err
+        })
+    },
+
+  mounted() {
+    let myChart = this.$echarts.init(document.getElementById("main1")); // 初始化实例 {
+    myChart.setOption({
+      title: {
+        text: "大佬进阶班",
+      },
+      tooltip: {},
+      xAxis: {
+        data: ["一班", "二班", "三班", "四班", "五班", "六班"],
+      },
+      yAxis: {},
+      series: [
+        {
+          name: "人数",
+          type: "bar",
+          data: [45, 42, 46, 48, 52, 47],
+        },
+      ],
+    });
+  },
+
+  methods: {
+    draw(legend, xAxis, series) {
+        let myChart1 = this.$echarts.init(document.getElementById("main2"));
+        let option = {
+            title: {
+                text: '会话量'
+            },
+            tooltip: {
+                trigger: 'axis'
+           },
+           legend: {
+                data: legend
+           },
+           xAxis: {
+                type: 'category',
+                data: xAxis
+           },
+           yAxis: {
+                type: 'value'
+           },
+           series: series
+        }
+        myChart1.setOption(option)
+    }
+  },
+};
+</script>
+
+<style lang="scss">
+.data-view {
+  width: 100%;
+  display: flex;
+  justify-content: space-between; // 两边靠
+  .el-card {
+    width: 50%;
+    margin: 15px;
+    #main1,#main2 {
+      height: 500px;
+    }
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,16 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
+
 // import '../plugins/element.js'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import 'font-awesome/css/font-awesome.min.css'
+
 // import axios from 'axios'
 import service from './service'
 import router from './router'
 
+import echarts from 'echarts'
+
 Vue.use(ElementUI);
 // Vue.prototype.axios = axios // 挂载到原型,可以在全局使用
 Vue.prototype.service = service 
+Vue.prototype.$echarts = echarts 
 Vue.config.productionTip = false
 
 
