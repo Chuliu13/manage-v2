9ae0e410eb5322335ccb38c9bac0bb8c92449549
Chuliu13 <2311629049@qq.com>
2022-07-03 16:14:44 +0800

    6.3作业列表

--- src/components/students/WorkList.vue
+++ src/components/students/WorkList.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <el-table :data="tableData" style="width: 100%">
+  <div class="workList">
+    <el-table :data="tableData" v-loading="loading" style="width: 100%">
       <el-table-column prop="id" label="用户ID" align="center">
       </el-table-column>
       <el-table-column prop="userId" label="所属班级" align="center">
@@ -10,24 +10,57 @@
       <el-table-column prop="completed_text" label="完成情况" align="center">
       </el-table-column>
     </el-table>
+    <el-pagination
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="page"
+      :page-sizes="[5, 10, 20, 50, 100]"
+      :page-size="size"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="total"
+    >
+    </el-pagination>
   </div>
 </template>
 <script>
-import { getTableData } from '@/utils/table'
+import { getTableData } from "@/utils/table";
 export default {
   data() {
     return {
-        tableData: []
-        // {
-        //     id:'',            
-        //     userId:'',
-        //     title:'',
-        //     completed:''
-        // }
-    }
+      tableData: [],
+      total: 0,
+      page: 1,
+      size: 10,
+      loading: true,
+    };
   },
-  created(){
-    getTableData(this, '/works',{} ,['completed'])
-  }
+  created() {
+    getTableData(this, "/works", { page: this.page, size: this.size }, [
+      "completed",
+    ]);
+  },
+  methods: {
+    handleSizeChange(val) {
+      this.szie = val;
+      this.page = 1;
+      getTableData(this, "/works", { page: this.page, size: val }, [
+        "completed",
+      ]);
+    },
+    handleCurrentChange(val) {
+      this.page = val;
+      getTableData(this, "/works", { page: val, size: this.size }, [
+        "completed",
+      ]);
+    },
+  },
 };
 </script>
+<style lang="scss">
+.workList {
+  .el-pagination {
+    text-align: left;
+    margin-top: 20px;
+  }
+}
+</style>
--- src/components/students/WorkMent.vue
+++ src/components/students/WorkMent.vue
@@ -1,5 +1,66 @@
 <template>
-    <div>
-        作业管理
-    </div>
+  <div class="workMent">
+    <el-table :data="tableData" v-loading="loading" style="width: 100%">
+      <el-table-column prop="id" label="用户ID" align="center">
+      </el-table-column>
+      <el-table-column prop="userId" label="所属班级" align="center">
+      </el-table-column>
+      <el-table-column prop="title" label="作业名称" align="center">
+      </el-table-column>
+      <el-table-column prop="completed_text" label="完成情况" align="center">
+      </el-table-column>
+    </el-table>
+    <el-pagination
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="page"
+      :page-sizes="[5, 10, 20, 50, 100]"
+      :page-size="size"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="total"
+    >
+    </el-pagination>
+  </div>
 </template>
+<script>
+import { getTableData } from "@/utils/table";
+export default {
+  data() {
+    return {
+      tableData: [],
+      total: 0,
+      page: 1,
+      size: 10,
+      loading: true,
+    };
+  },
+  created() {
+    getTableData(this, "/works", { page: this.page, size: this.size }, [
+      "completed",
+    ]);
+  },
+  methods: {
+    handleSizeChange(val) {
+      this.szie = val;
+      this.page = 1;
+      getTableData(this, "/works", { page: this.page, size: val }, [
+        "completed",
+      ]);
+    },
+    handleCurrentChange(val) {
+      this.page = val;
+      getTableData(this, "/works", { page: val, size: this.size }, [
+        "completed",
+      ]);
+    },
+  },
+};
+</script>
+<style lang="scss">
+.workMent {
+  .el-pagination {
+    text-align: left;
+    margin-top: 20px;
+  }
+}
+</style>
